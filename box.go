45f1815650c1788b54b8c3ddbde16e24ae3151e1
Max Reshetnikov <maxim.a.reshetnikov@gmail.com>
2022-05-28 01:43:05 +0400

    homework-6.1 ver 10

--- box.go
+++ box.go
@@ -85,16 +85,19 @@ func (b *box) SumArea() float64 {
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	exist := false
-	for i, v := range b.shapes {
-		_, ok := v.(Rectangle)
+	//exist := false
+	var resultShapes []Shape
+	for _, v := range b.shapes {
+		_, ok := v.(Circle)
 		if ok {
-			exist = true
-			b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
+			//exist = true
+		} else {
+			//resultShapes = append(resultShapes, v)
 		}
 	}
-	if !exist {
+	/*if !exist {
 		return errors.New("circles not found in box")
-	}
+	}*/
+	b.shapes = resultShapes
 	return nil
 }
